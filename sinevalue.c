18bb21b66c9faa0f388e7ad2851f2e1103a58df0
Aya Mouate <122039872+AYLA71@users.noreply.github.com>
2023-10-06 00:24:23 +0100

    Add files via upload

---
+++ sinevalue.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#define PI 3.14159
+
+int main(void)
+{
+     double radius;
+     printf("Enter radius:");
+     scanf("%lf", &radius);
+     printf("volume is : %lf \n\n", 4*PI*radius*radius*radius/3 );
+     return 0;
+}
